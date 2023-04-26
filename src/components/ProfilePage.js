58126e6d14e8447fe3961c3f99240986ad0f7998
Tnaike <timmynaike@gmail.com>
2023-04-27 00:52:35 +0100

    Fetching data with axios

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,10 +1,31 @@
-import Nav from "./Nav";
+import Nav from './Nav';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const ProfilePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/user').then((response) => {
+      setData(response.data);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <>
-    <Nav />
-      <h3>Profile Page</h3>
+      {isLoading ? (
+        <h2 className='loading'>Loading...</h2>
+      ) : (
+        <>
+          <Nav />
+          <h3>Profile Page</h3>
+          {data.map((user) => {
+            return <div key={user.id}>{user.name}</div>;
+          })}
+        </>
+      )}
     </>
   );
 };
