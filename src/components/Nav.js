2e2890de8cfc32986eb21dca31c15189adbdc56b
Tnaike <timmynaike@gmail.com>
2023-04-27 00:26:15 +0100

    Update nav component with style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-// import logo from '../assets/logo.png';
+import logo from '../assets/mrq_logo.png';
 import '../styles/index.css';
 
 const Nav = () => {
   return (
     <div className='nav-wrap'>
       <nav className='nav container'>
-        {/* <div className='logo'>
+        <div className='logo'>
           <img src={logo} alt='MRQ' />
-        </div> */}
+        </div>
         <ul>
           <li>
             <NavLink to='/'>Home</NavLink>
