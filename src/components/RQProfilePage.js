96979e27354d2d8475da743273f62d5a02b59ded
Tnaike <timmynaike@gmail.com>
2023-04-29 23:35:34 +0100

    Complete fetching data with useQuery

--- src/components/RQProfilePage.js
+++ src/components/RQProfilePage.js
@@ -1,10 +1,27 @@
 import Nav from './Nav';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchUsersProfile = () => {
+  return axios.get('http://localhost:4000/users');
+};
 
 const RQProfilePage = () => {
+  const { isLoading, data } = useQuery('users-profile', fetchUsersProfile);
+
   return (
     <>
-      <Nav />
-      <h3>RQ Profile Page</h3>
+      {isLoading ? (
+        <h2 className='loading'>Loading...</h2>
+      ) : (
+        <>
+          <Nav />
+          <h3>RQ Profile Page</h3>
+          {data?.data.map((user) => {
+            return <div key={user.id}>{user.name}</div>;
+          })}
+        </>
+      )}
     </>
   );
 };
