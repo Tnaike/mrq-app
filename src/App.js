4e1ead23ac0642cda1fce18c5d9180d20bfab5c1
Tnaike <timmynaike@gmail.com>
2023-04-29 20:56:42 +0100

    Setup react query

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import './App.css';
 import HomePage from './components/HomePage';
 import ProfilePage from './components/ProfilePage';
 import RQProfilePage from './components/RQProfilePage';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/rq-profile' element={<RQProfilePage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/rq-profile' element={<RQProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
