d5c51a3572b2594807170d0c73e20d094f54e0b6
Tnaike <timmynaike@gmail.com>
2023-05-05 00:35:55 +0100

    Query by id using useQuery hook

--- src/components/RQUserProfile.js
+++ src/components/RQUserProfile.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useUserData } from '../hooks/useUserData';
+import Nav from './Nav';
+import Loader from './Loader';
 
 const RQUserProfile = () => {
+  const { userId } = useParams();
+  const { isLoading, data: user, isError, error } = useUserData(userId);
+
   return (
     <>
-      <div className='container'>
-        <div className='page-header'>
-          <h3 className='mb-1'>RQ Users Page</h3>
-          <p>Using reactQuery</p>
-        </div>
-      </div>
+      <Nav />
+      {isLoading ? (
+        <Loader label />
+      ) : (
+        <>
+          {isError && (
+            <div className='errorMessage container'>
+              <h3>{error.message}</h3>
+            </div>
+          )}
+          <div className='container'>
+            <div className='page-header'>
+              <h3 className='mb-1'>RQ Users Page</h3>
+              <p>Using reactQuery</p>
+            </div>
+            <div className='card-cs'>
+              <p className='userEmail'>{user?.data.email}</p>
+              <p className='userLocation'>{user?.data.location}</p>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
