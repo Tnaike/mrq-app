c6cd123da58747403fd42dd12f54c416ce705d28
Tnaike <timmynaike@gmail.com>
2023-05-12 19:52:05 +0100

    user profile card layout

--- src/components/RQUserProfile.js
+++ src/components/RQUserProfile.js
@@ -3,6 +3,10 @@ import { useParams } from 'react-router-dom';
 import { useUserData } from '../hooks/useUserData';
 import Nav from './Nav';
 import Loader from './Loader';
+import { getStatus } from '../utils';
+import getProfileImage from '../utils/getProfileImage';
+import defaultImage from '../assets/user.png';
+import '../styles/index.css';
 
 const RQUserProfile = () => {
   const { userId } = useParams();
@@ -22,17 +26,36 @@ const RQUserProfile = () => {
           )}
           <div className='container'>
             <div className='page-header'>
-              <h3 className='mb-1'>RQ Users Page</h3>
+              <h3 className='mb-1'>RQ {user?.data?.name || 'User'} Profile</h3>
               <p>Using reactQuery</p>
             </div>
-            <div className='card-details mt-2'>
-              <h3 className='userName'>{user.data.name}</h3>
-              <p className='userPosition'>{user.position}</p>
-              <div className='card-cs'>
-                <p className='userEmail'>{user?.data.email}</p>
-                <p className='userLocation'>{user?.data.location}</p>
+            <section>
+              <div className='cards'>
+                <div className='card-img'>
+                  <img
+                    src={user?.data.profileImage || defaultImage}
+                    alt={user.data.name}
+                    onError={(e) => {
+                      e.target.src = defaultImage;
+                    }}
+                  />
+                  <div className='user-Status'>
+                    <span
+                      className={getStatus(user?.data?.status)}
+                      title={user?.data.status}
+                    ></span>
+                  </div>
+                </div>
+                <div className='card-details mt-2'>
+                  <h3 className='userName'>{user.data.name}</h3>
+                  <p className='userPosition'>{user.position}</p>
+                  <div className='card-cs'>
+                    <p className='userEmail'>{user?.data.email}</p>
+                    <p className='userLocation'>{user?.data.location}</p>
+                  </div>
+                </div>
               </div>
-            </div>
+            </section>
           </div>
         </>
       )}
